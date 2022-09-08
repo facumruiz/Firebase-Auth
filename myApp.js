03b3c7a5a2a80b16253d219599d65264b729b8e4
facumruiz <facundoruiz105@gmail.com>
2022-09-08 11:26:44 -0300

    subo form de logueo con firebase

---
+++ myApp.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let app = express();
+
+var bGround = require('fcc-express-bground');
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.route('/_api/package.json')
+  .get(function(req, res, next) {
+    console.log('requested');
+    fs.readFile(__dirname + '/package.json', function(err, data) {
+      if (err) return next(err);
+      res.type('txt').send(data.toString());
+    });
+  });
+
+app.route('/')
+  .get(function(req, res) {
+    res.sendFile(process.cwd() + '/views/registro.html');
+  })
+
+app.get('/registro', function(req, res) {
+  res.sendFile(__dirname + '/views/registro.html');
+})
+
+module.exports = app;
